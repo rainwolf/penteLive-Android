3b7eec8363e88567eed7b617d4ee5fdd76cc19a6
rainwolf <rainwolf@submanifold.be>
2024-01-27 19:42:51 +0800

    Update MyFcmListenerService.java

--- app/src/main/java/be/submanifold/pentelive/MyFcmListenerService.java
+++ app/src/main/java/be/submanifold/pentelive/MyFcmListenerService.java
@@ -14,8 +14,10 @@ import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
@@ -27,10 +29,12 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.Map;
 
@@ -53,56 +57,77 @@ public class MyFcmListenerService extends FirebaseMessagingService {
             System.out.println("Refreshed token: " + refreshedToken);
             System.out.println("Refreshed token: " + newToken);
             // TODO: Implement this method to send any registration to your app's servers.
-            sendRegistrationToServer(refreshedToken);
+            new SendRegistrationTask(refreshedToken).execute();
         }
         );
     }
-    // [END refresh_token]
 
-    private void sendRegistrationToServer(String token) {
-        String storedUserName = PrefUtils.getFromPrefs(MyFcmListenerService.this, PrefUtils.PREFS_LOGIN_USERNAME_KEY, null);
-        String storedPassword = PrefUtils.getFromPrefs(MyFcmListenerService.this, PrefUtils.PREFS_LOGIN_PASSWORD_KEY, null);
-        String storedToken = PrefUtils.getFromPrefs(MyFcmListenerService.this, PrefUtils.PREFS_TOKEN_KEY, "");
+    public class SendRegistrationTask extends AsyncTask<Void, Void, Boolean> {
 
-        Date date = new Date(System.currentTimeMillis()); //or simply new Date();
-        long millisNow = date.getTime();
-        if ((storedPassword != null && storedUserName != null) || !storedToken.equals(token)) {
-            try {
-                PrefUtils.saveLongToPrefs(MyFcmListenerService.this, PrefUtils.PREFS_TOKENLASTSENT_KEY, 0);
-                URL url = new URL("https://www.pente.org/gameServer/notification?device=android&token=" + token);
-                if (PentePlayer.development) {
-                    url = new URL("https://10.0.2.2/gameServer/notification?device=android&token=" + token);
-                }
-//                URL url = new URL("https://www.pente.org/gameServer/notifications/registerDeviceAndroid.jsp?name=" + storedUserName + "&password=" + storedPassword
-//                        + "&token=" + token);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                int responseCode = connection.getResponseCode();
-                if (responseCode != 200) {
-                    System.out.println("response code for submit was " + responseCode);
-                }
+        private final String token;
 
-                StringBuilder output = new StringBuilder();
-                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                System.out.println("output===============" + br);
-                String line = "";
-                while ((line = br.readLine()) != null) {
-                    output.append(line + "\n");
-                }
-                br.close();
-
-                if (output.toString().contains("It seems to have worked")) {
-                    PrefUtils.saveLongToPrefs(MyFcmListenerService.this, PrefUtils.PREFS_TOKENLASTSENT_KEY, millisNow);
-                    PrefUtils.saveToPrefs(MyFcmListenerService.this, PrefUtils.PREFS_TOKEN_KEY, token);
-                }
-
-
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
+        SendRegistrationTask(String token) {
+            this.token = token;
         }
 
-        // Add custom implementation, as needed.
+        @Override
+        protected Boolean doInBackground(Void... params) {
+
+            String storedUserName = PrefUtils.getFromPrefs(MyFcmListenerService.this, PrefUtils.PREFS_LOGIN_USERNAME_KEY, null);
+            String storedPassword = PrefUtils.getFromPrefs(MyFcmListenerService.this, PrefUtils.PREFS_LOGIN_PASSWORD_KEY, null);
+            String storedToken = PrefUtils.getFromPrefs(MyFcmListenerService.this, PrefUtils.PREFS_TOKEN_KEY, "");
+
+            Date date = new Date(System.currentTimeMillis()); //or simply new Date();
+            long millisNow = date.getTime();
+            if ((storedPassword != null && storedUserName != null) || !storedToken.equals(this.token)) {
+                try {
+                    PrefUtils.saveLongToPrefs(MyFcmListenerService.this, PrefUtils.PREFS_TOKENLASTSENT_KEY, 0);
+                    URL url = new URL("https://www.pente.org/gameServer/notification?device=android&token=" + this.token);
+                    if (PentePlayer.development) {
+                        url = new URL("https://10.0.2.2/gameServer/notification?device=android&token=" + this.token);
+                    }
+//                URL url = new URL("https://www.pente.org/gameServer/notifications/registerDeviceAndroid.jsp?name=" + storedUserName + "&password=" + storedPassword
+//                        + "&token=" + token);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    int responseCode = connection.getResponseCode();
+                    if (responseCode != 200) {
+                        System.out.println("response code for submit was " + responseCode);
+                    }
+
+                    StringBuilder output = new StringBuilder();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    System.out.println("output===============" + br);
+                    String line = "";
+                    while ((line = br.readLine()) != null) {
+                        output.append(line + "\n");
+                    }
+                    br.close();
+
+                    if (output.toString().contains("It seems to have worked")) {
+                        PrefUtils.saveLongToPrefs(MyFcmListenerService.this, PrefUtils.PREFS_TOKENLASTSENT_KEY, millisNow);
+                        PrefUtils.saveToPrefs(MyFcmListenerService.this, PrefUtils.PREFS_TOKEN_KEY, this.token);
+                    }
+
+
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+        }
+
+        @Override
+        protected void onCancelled() {
+        }
     }
+
+    // [END refresh_token]
 
     @Override
     public void onMessageReceived(RemoteMessage message) {
